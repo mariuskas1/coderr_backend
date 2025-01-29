9d9a0874e1288fa4d4eb802520304e231771e5f2
Marius Kasparek <marius.kasparek@gmail.com>
2025-01-29 15:53:43 +0100

    added DetailsViewset and improved OfferSerializer

--- coderr_app/api/serializers.py
+++ coderr_app/api/serializers.py
@@ -10,8 +10,35 @@ class DetailsSerializer(serializers.ModelSerializer):
 
 
 class OfferSerializer(serializers.ModelSerializer):
-    details = DetailsSerializer(many = False, required = False)
+    details = DetailsSerializer(many=True, required=False)
 
     class Meta:
         model = Offer
         fields = '__all__'
+
+    def create(self, validated_data):
+        details_data = validated_data.pop('details', [])  # Extract details
+        offer = Offer.objects.create(**validated_data)  # Create Offer
+
+        for detail in details_data:
+            Details.objects.create(offer=offer, **detail)  # Create related Details
+
+        return offer
+
+    def update(self, instance, validated_data):
+        details_data = validated_data.pop('details', None)
+
+        # Update Offer fields
+        instance.title = validated_data.get('title', instance.title)
+        instance.image = validated_data.get('image', instance.image)
+        instance.description = validated_data.get('description', instance.description)
+        instance.min_price = validated_data.get('min_price', instance.min_price)
+        instance.min_delivery_time = validated_data.get('min_delivery_time', instance.min_delivery_time)
+        instance.save()
+
+        if details_data is not None:
+            instance.details.all().delete()  # Remove old details
+            for detail_data in details_data:
+                Details.objects.create(offer=instance, **detail_data)  # Add new details
+
+        return instance
--- coderr_app/api/views.py
+++ coderr_app/api/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets, generics, permissions
-from coderr_app.models import Offer
+from coderr_app.models import Offer, Details
 from .serializers import OfferSerializer, DetailsSerializer
 
+
+class DetailsViewSet(viewsets.ModelViewSet):
+    queryset = Details.objects.all()
+    serializer_class = DetailsSerializer
 
 class OfferViewset(viewsets.ModelViewSet):
     queryset = Offer.objects.all()
